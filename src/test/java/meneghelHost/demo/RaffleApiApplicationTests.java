9672e872cf888aecac459f376d2445dd4231ab73
Derek <derekmeneghel2004@gmail.com>
2025-12-17 21:17:13 -0300

    first commit

---
+++ src/test/java/meneghelHost/demo/RaffleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package meneghelHost.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaffleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
